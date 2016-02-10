1bed51aa2e7cf0ecc38849eabb8e1b9d35c7d250
rarcega <rarcega@users.noreply.github.com>
2016-02-10 00:37:18 -0500

    fixed querystring causing downloads to fail; added progress meter; removed args parser

--- app.py
+++ app.py
@@ -4,19 +4,19 @@
 Usage: 
 python app.py <username>
 """
+import concurrent.futures
 import json
 import os
 import requests
 import sys
-import concurrent.futures
-import argparse
-import datetime
+from tqdm import tqdm
+
 
 def crawl(username, items=[], max_id=None):
-    url   = 'http://instagram.com/' + username + '/media' + ('?&max_id=' + max_id if max_id is not None else '')
+    url = 'http://instagram.com/' + username + '/media' + ('?&max_id=' + max_id if max_id is not None else '')
     media = json.loads(requests.get(url).text)
 
-    items.extend( [ curr_item for curr_item in media['items'] ] )
+    items.extend([curr_item for curr_item in media['items']])
 
     if 'more_available' not in media or media['more_available'] is False:
         return items
@@ -24,44 +24,32 @@ def crawl(username, items=[], max_id=None):
         max_id = media['items'][-1]['id']
         return crawl(username, items, max_id)
 
+
 def download(item, save_dir='./'):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
     url = item[item['type'] + 's']['standard_resolution']['url']
-    base_name = url.split('/')[-1]
-    if args.prefix_created_time_format:
-      base_name = datetime.datetime.fromtimestamp(
-                      int(item['created_time'])
-                  ).strftime(args.prefix_created_time_format) + base_name
+    base_name = url.split('/')[-1].split('?')[0]
     file_path = os.path.join(save_dir, base_name)
 
     with open(file_path, 'wb') as file:
-        print 'Downloading ' + base_name
         bytes = requests.get(url).content
         file.write(bytes)
 
+
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(add_help=True)
-  parser.add_argument('username')
-  parser.add_argument('--prefix-created-time',
-                      nargs='?',
-                      const='%Y%m%dT%H%M%S_',
-                      dest='prefix_created_time_format',
-                      help='Prefix the media filename with the specified datetime format')
-  parser.add_argument('args', nargs=argparse.REMAINDER)
+    username = sys.argv[1]
 
-  global args
-  args = parser.parse_args()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_item = {}
+        for item in crawl(username):
+            future = executor.submit(download, item, './' + username)
+            future_to_item[future] = item
 
+        for future in tqdm(concurrent.futures.as_completed(future_to_item), total=len(future_to_item)):
+            item = future_to_item[future]
+            url = item[item['type'] + 's']['standard_resolution']['url']
 
-  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-    future_to_item = dict( (executor.submit(download, item, './' + args.username), item) for item in crawl(args.username) )
-  
-    for future in concurrent.futures.as_completed(future_to_item):
-      item = future_to_item[future]
-      url  = item[item['type'] + 's']['standard_resolution']['url']
-      
-      if future.exception() is not None:
-        print '%r generated an exception: %s' % (url, future.exception())
-
+            if future.exception() is not None:
+                print ('%r generated an exception: %s') % (url, future.exception())
