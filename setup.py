1df054efcb89207a75bc51371b967a85322d17b9
Nace Oroz <orkica@gmail.com>
2016-11-08 05:13:04 +0100

    Transforming the project into a proper packageable layout (#22)
    * Transforming the project into a proper packagable layout
    
    * travis install step changed
    
    * we're not python 3 compatible yet

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='instagram-scraper',
+      version='0.1',
+      description=("instagram-scraper is a command-line application written in Python"
+                    "that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
+      url='https://github.com/rarcega/instagram-scraper',
+      author='Richard Arcega',
+      license='Public domain',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=["requests>=1.0.4",
+                        "futures>=2.2.0",
+                        "tqdm>=3.8.0"],
+      entry_points = {
+        'console_scripts': ['instagram-scraper=instagram_scraper.app:main'],
+      },
+      zip_safe=False)
