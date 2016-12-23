38c6cfdecb9d254a850c20d11490e0177b62cfb5
rarcega <rarcega@users.noreply.github.com>
2016-12-22 23:22:38 -0500

    changed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='instagram-scraper',
-      version='0.1',
+      version='1.0.0',
       description=("instagram-scraper is a command-line application written in Python"
                     "that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
       url='https://github.com/rarcega/instagram-scraper',
       author='Richard Arcega',
+      author_email='hello@richardarcega.com',
       license='Public domain',
       packages=find_packages(exclude=['tests*']),
       install_requires=["requests>=1.0.4",
@@ -17,4 +18,5 @@ setup(name='instagram-scraper',
         'console_scripts': ['instagram-scraper=instagram_scraper.app:main'],
       },
       test_suite = 'nose.collector',
-      zip_safe=False)
+      zip_safe=False,
+      keywords=['instagram', 'scraper', 'download', 'media', 'photos', 'videos'])
