85014adad4fb288c0d7f747b55c794bfc8462b38
Daniel M. Capella <polyzen@users.noreply.github.com>
2017-05-02 23:40:49 -0400

    Python >=3.2 ships concurrent.futures (#44)
    Taken from requests-futures,
    
    Includes fixes for errors C0330, E127, and E251.

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
+import sys
 from setuptools import setup, find_packages
 
-setup(name='instagram-scraper',
-      version='1.2.1',
-      description=("instagram-scraper is a command-line application written in Python"
-                    " that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
-      url='https://github.com/rarcega/instagram-scraper',
-      download_url='https://github.com/rarcega/instagram-scraper/tarball/1.2.1',
-      author='Richard Arcega',
-      author_email='hello@richardarcega.com',
-      license='Public domain',
-      packages=find_packages(exclude=['tests']),
-      install_requires=["requests>=1.0.4",
-                        "futures==2.2.0",
-                        "tqdm>=3.8.0",
-                        "requests_mock",
-                        "nose"],
-      entry_points = {
+requires = [
+    'requests>=1.0.4',
+    'tqdm>=3.8.0',
+    'requests_mock',
+    'nose'
+]
+
+if sys.version_info < (3, 2):
+    requires.append('futures==2.2')
+
+setup(
+    name='instagram-scraper',
+    version='1.2.1',
+    description=("instagram-scraper is a command-line application written in Python"
+                 " that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
+    url='https://github.com/rarcega/instagram-scraper',
+    download_url='https://github.com/rarcega/instagram-scraper/tarball/1.2.1',
+    author='Richard Arcega',
+    author_email='hello@richardarcega.com',
+    license='Public domain',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requires,
+    entry_points={
         'console_scripts': ['instagram-scraper=instagram_scraper.app:main'],
-      },
-      test_suite='nose.collector',
-      zip_safe=False,
-      keywords=['instagram', 'scraper', 'download', 'media', 'photos', 'videos'])
+    },
+    test_suite='nose.collector',
+    zip_safe=False,
+    keywords=['instagram', 'scraper', 'download', 'media', 'photos', 'videos']
+)
